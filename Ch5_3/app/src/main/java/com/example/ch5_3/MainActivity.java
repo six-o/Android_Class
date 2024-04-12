97315484f75cd48a53e78dea678b83c61a53000e
Six <134152000+six-o@users.noreply.github.com>
2024-04-12 19:39:36 +0800

    0411_Ch5_3_Debug

--- Ch5_3/app/src/main/java/com/example/ch5_3/MainActivity.java
+++ Ch5_3/app/src/main/java/com/example/ch5_3/MainActivity.java
@@ -1,26 +1,28 @@
 package com.example.ch5_3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
-    private float original_size = 20;
-
-
+    private float original_size = 30;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView txtOutput = (TextView) findViewById(R.id.txtOutput);
-        txtOutput.setTextSize(original_size);
+        txtOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX, original_size);
+        Log.v("SIZE", "initializeSize = " +original_size + " px");
         Button btnEnlarge = (Button) findViewById(R.id.btnEnlarge);
         btnEnlarge.setOnClickListener(this);
         btnEnlarge.setOnLongClickListener(this);
@@ -30,14 +32,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         TextView txtOutput = (TextView) findViewById(R.id.txtOutput);
         float size = txtOutput.getTextSize();
-        Log.v("SIZE", "size = " + size);
-        txtOutput.setTextSize(size + 1);
+        float newSize = size + 10; // 假設增加 10 個像素
+        Log.v("SIZE", "size = " + newSize + " px");
+        txtOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX, newSize);
     }
 
     @Override
     public boolean onLongClick(View v) {
         TextView txtOutput = (TextView) findViewById(R.id.txtOutput);
-        txtOutput.setTextSize(original_size);
+        Log.v("SIZE", "size = " +original_size + " px");
+        txtOutput.setTextSize(TypedValue.COMPLEX_UNIT_PX, original_size);
         return false;
     }
 }
